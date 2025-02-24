dd3682126b0bd83bfdf71c63dcd61760fa2c712f
luoheyuan <heyuan.luo@gmail.com>
2025-02-24 15:42:33 +0800

    增加分页

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,6 +16,14 @@ const data: ISiteMetadataResult = {
   description: 'Personal site and blog',
   navLinks: [
     {
+      name: 'Run',
+      url: 'https://sisyplone.github.io/running_page',
+    },
+    {
+      name: 'Cycling',
+      url: 'https://sisyplone.github.io/cycling_page',
+    },
+    {
       name: 'About',
       url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
     },
