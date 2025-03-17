72cac588edada4a2ec8cb1ff9f39e2ac2b5f0d90
luoheyuan <heyuan.luo@gmail.com>
2025-03-17 11:33:31 +0800

    close only run

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,16 +16,8 @@ const data: ISiteMetadataResult = {
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Run',
-      url: 'https://sisyplone.github.io/running_page',
-    },
-    {
-      name: 'Cycling',
-      url: 'https://sisyplone.github.io/cycling_page',
-    },
-    {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/Sisyplone/running_page/blob/master/README-CN.md',
     },
   ],
 };
