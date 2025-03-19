9955327bf1d82df524bcf68ad53814b9e363e9eb
luoheyuan <heyuan.luo@gmail.com>
2025-03-19 11:14:26 +0800

    fix:将数据强制刷新的开关放到环境变量中

--- run_page/strava_sync.py
+++ run_page/strava_sync.py
@@ -7,13 +7,12 @@ from generator import Generator
 
 
 # for only run type, we use the same logic as garmin_sync
-def run_strava_sync(client_id, client_secret, refresh_token, only_run=False):
-    refresh_force = os.getenv("REFRESH_FORCE", "False").lower() == "true"
+def run_strava_sync(client_id, client_secret, refresh_token, refresh_force="False", only_run=False):
     generator = Generator(SQL_FILE)
     generator.set_strava_config(client_id, client_secret, refresh_token)
     # if you want to refresh data change False to True
     generator.only_run = only_run
-    generator.sync(refresh_force)
+    generator.sync(refresh_force.lower() == "true")
 
     activities_list = generator.load()
     with open(JSON_FILE, "w") as f:
@@ -25,6 +24,7 @@ if __name__ == "__main__":
     parser.add_argument("client_id", help="strava client id")
     parser.add_argument("client_secret", help="strava client secret")
     parser.add_argument("refresh_token", help="strava refresh token")
+    parser.add_argument("refresh_force", help="force refresh data")
     parser.add_argument(
         "--only-run",
         dest="only_run",
@@ -36,5 +36,6 @@ if __name__ == "__main__":
         options.client_id,
         options.client_secret,
         options.refresh_token,
+        options.refresh_force,
         only_run=options.only_run,
     )
